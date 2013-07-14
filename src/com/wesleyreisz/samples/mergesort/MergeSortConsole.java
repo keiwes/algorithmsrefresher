a8e13f37de88c86380d0bc0b466820fcb335f67f
Wesley Reisz <wes@wesleyreisz.com>
2013-07-14 16:31:08 -0400

    updated input range to large numbers

--- src/com/wesleyreisz/samples/mergesort/MergeSortConsole.java
+++ src/com/wesleyreisz/samples/mergesort/MergeSortConsole.java
@@ -2,8 +2,20 @@ package com.wesleyreisz.samples.mergesort;
 
 public class MergeSortConsole {
 
+	private static final int COUNT = 500;
 	public static void main(String[] args) {
-		int[] values = {21,5,0,7,-5,7,34,10,11,-56};
+		int[] values = new int[COUNT];
+		
+		for(int i = 0; i<COUNT;i++){
+			values[i] = (int) ((Math.random()*COUNT) + 1);
+		}
+		
+		System.out.println("\nPresort: ");
+		for(int value: values){
+			System.out.print(value + " ");
+		}
+		
+		System.out.println("\nSorted: ");
 		for(int value: MergeSort.sort(values)){
 			System.out.print(value + " ");
 		}	
